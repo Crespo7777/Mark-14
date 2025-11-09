7a3d2f6ac9a133f201be055eaab934f8954bd04a
Crespo7777 <projettosrpg@gmail.com>
2025-11-09 14:17:35 -0300

    Create character.constants.ts

---
+++ src/features/character/character.constants.ts
@@ -0,0 +1,12 @@
+// src/features/character/character.constants.ts
+
+export const attributesList: { key: string; label: string }[] = [
+  { key: "cunning", label: "Astuto" },
+  { key: "quick", label: "Rápido" },
+  { key: "discreet", label: "Discreto" },
+  { key: "resolute", label: "Resoluto" },
+  { key: "persuasive", label: "Persuasivo" },
+  { key: "vigilant", label: "Vigilante" },
+  { key: "precise", label: "Preciso" },
+  { key: "vigorous", label: "Vigoroso" },
+];
